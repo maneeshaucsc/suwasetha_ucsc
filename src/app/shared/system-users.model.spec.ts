4814fb16fbfc41eb9269dd9aecf0427507dd59a3
Aathil <ibnusafina@gmail.com>
2020-01-09 17:47:07 +0530

    first upload by Aathil

---
+++ src/app/shared/system-users.model.spec.ts
@@ -0,0 +1,7 @@
+import { SystemUsers } from './system-users.model';
+
+describe('SystemUsers', () => {
+  it('should create an instance', () => {
+    expect(new SystemUsers()).toBeTruthy();
+  });
+});
