cf293cfa7ef104b6f759d644458a87862a8527a5
maneeshaucsc <36620408+maneeshaucsc@users.noreply.github.com>
2020-02-14 13:06:15 +0530

    doctornavbarcreated

---
+++ doctor-navbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DoctorNavbarComponent } from './doctor-navbar.component';
+
+describe('DoctorNavbarComponent', () => {
+  let component: DoctorNavbarComponent;
+  let fixture: ComponentFixture<DoctorNavbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DoctorNavbarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DoctorNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
