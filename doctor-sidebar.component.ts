84900ea46b7a9109c0494fd04f30a3749d0cfa4e
maneeshaucsc <36620408+maneeshaucsc@users.noreply.github.com>
2020-02-14 13:06:49 +0530

    doctorsidebar

---
+++ doctor-sidebar.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/appointments', title:'Appointments', icon:'person', class:''},
+    { path: '/prescriptions', title: 'Prescriptions',  icon:'person', class: '' },
+    { path: '/blog-posts', title: 'Blog Posts',  icon:'person', class: '' },
+    
+  
+];
+
+
+@Component({
+  selector: 'app-doctor-sidebar',
+  templateUrl: './doctor-sidebar.component.html',
+  styleUrls: ['./doctor-sidebar.component.scss']
+})
+export class DoctorSidebarComponent implements OnInit {
+
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+
+}
+
